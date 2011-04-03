37a166d0e54c20c6da699d2210af4ba2f509dd2c
Dylan Doxey <dylan@cpan.org>
2011-04-03 07:28:09 +0000

    Import of DYLAN/Filter-BoxString-0.05 from CPAN.
    gitpan-cpan-distribution: Filter-BoxString
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         DYLAN/Filter-BoxString-0.05.tar.gz
    gitpan-cpan-author:       DYLAN
    gitpan-cpan-maturity:     released

--- lib/Filter/BoxString.pm
+++ lib/Filter/BoxString.pm
@@ -1,16 +1,24 @@
 package Filter::BoxString;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use 5.008008;
 use strict;
 use warnings FATAL => 'all';
 use Filter::Simple;
 
-my $delimiter = 'BOXSTRING';
+my $Delimiter = 'FILTER_BOXSTRING';
+
+my $Scalar_Regex = qr/
+    [\$] \w+ (?: \{ [^\}]+ \} | \[ [^\]]+ \] )?
+/xms;
+
+my $Assignment_Regex = qr{
+    (?: my \s+ )? $Scalar_Regex \s* =
+}xms;
 
 my $BoxString_Regex = qr{
-    ( (?: my \s+ )? \$\w+ \s* = \s* [+] (?: .*? ) + [+]; )
+    ( $Assignment_Regex ) ( \s* [+] (?: .*? ) [+]; )
 }msx;
 
 FILTER_ONLY
@@ -18,51 +26,39 @@ executable => sub {
 
     while ( m/$BoxString_Regex/msxg ) {
 
-        my $boxstring = $1;
+        my $heredoc   = $1;
+        my $boxstring = $2;
 
-        my $boxstring_regex;
-        {
-            my $boxstring_re = $boxstring;
-            $boxstring_re =~ s{([\*\/\?\^\|\$\.+\-\\\(\)\[\]])}{\\$1}g;
-            $boxstring_regex = qr{$boxstring_re};
+        while ( $boxstring =~ m/$Delimiter/ ) {
+
+            $Delimiter .= int rand 999;
         }
 
-        # If the standard here-doc delimiter happens to be in the text
-        # then append ints to it until it's not in the text.
-        while ( $boxstring =~ m/$delimiter/ ) {
-            $delimiter .= int rand 9;
-        }
-
-        my ($here_doc) = $boxstring =~ m/( (?: my \s+ )? \$[\w\{\}\[\]]+ \s* = ) \s* [+]/msx;
-
-        $here_doc .= qq{ <<"$delimiter";\n};
+        $heredoc .= qq{ <<"$Delimiter";\n};
 
         my @lines = split /\n\s*[|]/, $boxstring;
 
         shift @lines;
-        
-        # strip the trailing +---+;
+
         if ( @lines ) {
+
+            # strip the trailing +---+;
             $lines[$#lines] =~ s{ [+-;]+ \z}{}msxg;
         }
 
         for my $line (@lines) {
 
-            if ( $line =~ s/ [|] \s* \z//msx ) {
+            if ( $line !~ s/ [|] \s* \z//msx ) {
 
-                $here_doc .= "$line\n";
-            }
-            else {
                 $line =~ s/ \s* \z//msx;
-
-                $here_doc .= "$line\n";
             }
+
+            $heredoc .= "$line\n";
         }
 
-        $here_doc .= $delimiter;
+        $heredoc .= $Delimiter;
 
-        # Swap out the box-string with the here-doc.
-        s/$boxstring_regex/$here_doc/;
+        s/\Q$boxstring\E/$heredoc/;
     }
 };
 
@@ -70,11 +66,9 @@ executable => sub {
 
 __END__
 
-=pod
-
 =head1 NAME
 
-Filter::BoxString - Describe your multiline strings as text boxes.
+Filter::BoxString - Describe your multiline strings as BoxStrings.
 
 =head1 SYNOPSIS
 
@@ -104,7 +98,6 @@ Filter::BoxString - Describe your multiline strings as text boxes.
                   |</shopping_list>
                   +---------------------------------------+;
 
-
     my $beatles = +
                   | Love Me Do
                   | I wanna hold your hand
@@ -131,40 +124,51 @@ Filter::BoxString - Describe your multiline strings as text boxes.
                   +------------------------------------------------------------+;
 
   my $gibberish = +-----------------------------------+
-                  | +!@#\$%^&*()_|"?><{}>~=-\'/.,[]
-                  | +=!@#$%^&*()_-|\"':;?/>.<,}]{[><~`
+                  | +!@#%^&*()_|"?><{}>~=-\'/.,[]
+                  | +=!@#%^&*()_-|\"':;?/>.<,}]{[><~`
                   +-----------------------------------+;
 
 
 =head1 DESCRIPTION
 
-This filter allows you to describe multiline strings in your code using ASCII art style boxes.
-Underneath it all, this filter transforms your box-string to the equivilent here-doc.
+This filter allows you to describe multiline strings in your code using ASCII
+art style BoxStrings. Underneath it all, this filter transforms your BoxString
+to the equivilent here-doc.
 
-The purpose is purely aesthetic. Please enjoy.
+The purpose is purely aesthetic.
+
+=head1 SYNTAX
+
+The BoxString instance must be some scalar assignment like:
+
+=over
+
+=item *
+
+    $identifier
+        = +---+
+          | x |
+          +---+;
+
+=item *
+
+    $ident{ifier} = +---+
+                    | x |
+                    +---+;
+
+=item *
+
+    $ident[$ifier] =
+        +---+
+        | x |
+        +---+;
+
+=back
+
 
 =head1 BUGS AND LIMITATIONS
 
-The methodology is to capture your boxstring by matching something like " = + ... +; " 
-which will break if your boxstring contains the sequence '+;'. 
-
-=head1 TODO
-
-Make the delimiters as flexible as perl. 
-
-Perhaps:
-
- my $s = qq---------------+
-          | "hello world" |
-          +---------------qq;
-
- my $s = "---------------+
-         | hello world+; |
-         +---------------";
-
- my $s = '------------------+
-         | qq{hello world}; |
-         +------------------';
+The text of your BoxString can't contain the sequence: +;
 
 =head1 AUTHOR
 
@@ -172,11 +176,8 @@ Dylan Doxey E<lt>dylan.doxey@gmail.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2008 by Dylan Doxey
+Copyright (C) 2011 by Dylan Doxey
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
 at your option, any later version of Perl 5 you may have available.
-
-=cut
-
